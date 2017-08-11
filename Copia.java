b38da04089d12df8b54c13e2f5fe240657d2ddf6
Othon Marcelo Nunes Batista <othon.batista@labinfo.unifacs.br>
2017-08-11 09:55:45 -0300

    Arquivo novo adicionado

---
+++ Copia.java
@@ -0,0 +1,15 @@
+public class Primeiro {
+	public static void main (String [] a) {
+		System.out.println("Isso e so um teste...");
+
+	}
+
+	private String nomeUsuario;
+
+	public String getUsuario ()  {
+
+		return this.nomeUsuario;
+
+	}
+
+}
